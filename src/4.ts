735fc37d75a6aa415938b28691d9683b58f155bc
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-03-12 16:55:56 +0100

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+interface IKey{
+    getSignature(): number;
+}
+class Key implements IKey{
+    private signature: number; 
+    constructor() {
+        this.signature= Math.random();
+    }
+    getSignature() {
+        return this.signature;
+    }
+}
 
+interface IPerson{
+     getKey(): Key;
+}
+class Person implements IPerson{
+    constructor(private key: Key) { };
+    getKey() {
+        console.log("Person got key", this.key);
+        return this.key;
+    }
+}
+
+abstract class House{
+    tenants: Person[]=[];
+  door: boolean;
+    key: Key;
+    comeIn(person: Person) {
+        if (this.door) {
+            console.log("Person come in");
+            
+          this.tenants.push(person);
+          console.log("tenants:",this.tenants);
+        }
+    }
+   abstract openDoor(key: Key): void;
+}
+class MyHouse extends House{
+      key: Key;
+  constructor(key: Key) {
+    super();
+    this.key = key;
+    }
+  openDoor(key: Key) {
+        if (key === this.key) {
+            console.log("Opened door");
+            this.door = true;
+    }
+}
+}
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
+
 
 
 export {};
